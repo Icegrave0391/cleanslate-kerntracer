cb79f90258af76742011288430ed61b524011b85
icegrave0391 <chuqiz@comp.nus.edu.sg>
2025-04-17 04:57:20 -0700

    x

--- guest/kernel/copy-build-source.sh
+++ guest/kernel/copy-build-source.sh
@@ -4,6 +4,8 @@ pushd ../ && source .env && popd
 # load the vm image (if not currently loaded)
 ./load-vmdisk.sh 
 
+log_info "Copying kernel build source to VMdisk /lib/modules/$GUEST_LINUX_VER/build"
+
 # guest Linux version
 GUEST_LINUX_VER=$KERNELVERSION
 
@@ -12,7 +14,8 @@ sudo unlink $VMDISKMOUNT/lib/modules/$GUEST_LINUX_VER/build || true
 
 # copy the updated build source
 sudo mkdir -p $VMDISKMOUNT/lib/modules/$GUEST_LINUX_VER/build
-sudo cp -ur linux/* $VMDISKMOUNT/lib/modules/$GUEST_LINUX_VER/build/
+# sudo cp -ur linux/* $VMDISKMOUNT/lib/modules/$GUEST_LINUX_VER/build/
+sudo rsync -a --update linux/ $VMDISKMOUNT/lib/modules/$GUEST_LINUX_VER/build/
 
 # unload the vm image
 ./unload-vmdisk.sh
